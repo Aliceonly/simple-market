80f6e5cd97c080fb4b360c89008f919ac6683a27
Aliceonly <aliceonly@foxmail.com>
2023-11-09 19:50:22 +0800

    update sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,35 +1,40 @@
-const path = require('path')
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config({ path: path.join(__dirname, '/.env.local') })
-require('hardhat-gas-reporter')
+const path = require("path");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: path.join(__dirname, "/.env.local") });
+require("hardhat-gas-reporter");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY, process.env.ACCOUNT2_PRIVATE_KEY],
       gas: 5500000,
-      gasPrice: 7000000000
+      gasPrice: 7000000000,
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY, process.env.ACCOUNT2_PRIVATE_KEY]
-    }
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY, process.env.ACCOUNT2_PRIVATE_KEY],
+    },
+    sepolia: {
+      url: "https://eth-sepolia.public.blastapi.io",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY, process.env.ACCOUNT2_PRIVATE_KEY],
+      chainId: 11155111,
+    },
   },
   solidity: {
-    version: '0.8.4',
+    version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+        runs: 200,
+      },
+    },
+  },
+};
